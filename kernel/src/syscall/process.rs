6d8f56d2c4428ce992edaf9a723b6a71961957e1
Muqiu Han <muqiu-han@outlook.com>
2023-01-07 15:44:22 +0800

    Fixed [KERNEL] [ERROR ]: Panicked at src/trap/mod.rs:39 Unsupported trap Exception(IllegalInstruction), stval = 0x0

--- kernel/src/syscall/process.rs
+++ kernel/src/syscall/process.rs
@@ -2,6 +2,6 @@ use crate::batch::run_next_app;
 
 /// print the return value of the exited application and call run_next_app to switch to the next application
 pub fn sys_exit(xstate: i32) -> ! {
-    error!("application exited with code {}!!!", xstate);
+    info!("application exited with code {}", xstate);
     run_next_app()
 }
